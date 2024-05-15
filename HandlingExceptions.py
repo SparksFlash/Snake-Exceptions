21a3be6f8f66071b008d8ae44ba77645cb873a44
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-15 12:24:00 +0600

    final

--- HandlingExceptions.py
+++ HandlingExceptions.py
@@ -1,4 +1,9 @@
 try:
     age = int(input("Age :"))
-except ValueError:
+except ValueError as ex:
     print("You didn't enter a valid age.")
+    print(ex)
+    print(type(ex))
+else:
+    print("No exceptions were thrown.")
+print("Execution continues.")
