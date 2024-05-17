45c23ef53995e4add02d1b5b09199c0658c42315
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-17 22:02:25 +0600

    consumed

--- HandlingExceptions.py
+++ HandlingExceptions.py
@@ -2,12 +2,12 @@ try:
     age = int(input("Age :"))
     xfactor = 10 / age
 
-except ValueError as ex:
+except (ValueError, ZeroDivisionError) as ex:
     print("You didn't enter a valid age.")
     print(ex)
     print(type(ex))
-except ZeroDivisionError:
-    print("Age can not be 0.")
+# except ZeroDivisionError:
+#     print("Age can not be 0.")
 else:
     print("No exceptions were thrown.")
 print("Execution continues.")
