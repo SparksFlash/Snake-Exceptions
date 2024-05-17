31d777301ab37d5bfdba33637303ae28a1bad34d
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-17 21:59:02 +0600

    zerodivisionerror

--- HandlingExceptions.py
+++ HandlingExceptions.py
@@ -6,6 +6,8 @@ except ValueError as ex:
     print("You didn't enter a valid age.")
     print(ex)
     print(type(ex))
+except ZeroDivisionError:
+    print("Age can not be 0.")
 else:
     print("No exceptions were thrown.")
 print("Execution continues.")
