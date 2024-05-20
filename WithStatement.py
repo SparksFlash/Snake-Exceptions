5ff2ee1e00da7883709d284b5a0c1e448de690f5
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-20 11:18:00 +0600

    With-statement

---
+++ WithStatement.py
@@ -0,0 +1,12 @@
+try:
+    with open("CleaningUp.py") as file:
+        print("File opened.")
+    age = int(input("Age :"))
+    xfactor = 10 / age
+
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+# except ZeroDivisionError:
+#     print("Age can not be 0.")
+else:
+    print("No exceptions were thrown.")
